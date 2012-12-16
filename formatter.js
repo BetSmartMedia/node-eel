e096414ea886d562b76d8e3670603f9df063da39
Stephen Sugden <glurgle@gmail.com>
2012-12-15 21:15:11 -0800

    Encode circular references using JSON path

--- formatter.js
+++ formatter.js
@@ -2,9 +2,12 @@ traverse = require('traverse')
 
 exports.safeJSON = safeJSON
 
+/**
+ * Encode possibly-circular JSON structures using JSON path urls
+ */
 function safeJSON (obj) {
   var scrubbed = traverse(obj).map(function () {
-    if (this.circular) this.update(this.path.join('.'))
+    if (this.circular) this.update('#/' + this.path.join('/'))
   })
   var string = JSON.stringify(scrubbed)
   if (string) string += "\n";
